9c7b6c1cf5711e0649b3ce8c8dbd65f94e2382a1
Alex Brown <alex.brown2@kingfisher.com>
2018-03-06 12:33:58 +0000

    Initial Commit

---
+++ ShelterUsers/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ShelterUsers
+//
+//  Created by Alex Brown on 06/03/2018.
+//  Copyright © 2018 Kingfisher digital. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
